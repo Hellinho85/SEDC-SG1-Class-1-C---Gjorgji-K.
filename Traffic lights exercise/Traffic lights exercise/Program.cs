a7ef5db02ff1e7b4b5827412fdc8bb7fa43adcf0
Gjorgji Kongulovski <46351282+Hellinho85@users.noreply.github.com>
2019-02-25 20:50:23 +0100

    Add files via upload

---
+++ Traffic lights exercise/Traffic lights exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_lights_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Traffic lights ... " + "\n");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red Light - STOP!" + "\n");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow Lights - GET READY!" + "\n");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green Lights - GO!" + "\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("And there they all go!");
+            
+
+        }
+    }
+}
